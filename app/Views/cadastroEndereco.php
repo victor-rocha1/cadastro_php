81e40557649e9ba7693da308d728fc1e41178244
VICTOR ROCHA DOS REIS <p813561@prodemge.gov.br>
2025-03-05 15:53:44 -0300

    mudança na estrutura do form

--- app/Views/cadastroEndereco.php
+++ app/Views/cadastroEndereco.php
@@ -12,26 +12,27 @@
     <form method="POST" action="index.php?action=cadastroEndereco">
 
         <input type="hidden" name="id_pessoa" value="<?php echo isset($_GET['id_pessoa']) ? $_GET['id_pessoa'] : ''; ?>">
-        <label>CEP:</label>
-        <input type="text" name="cep" required>
 
-        <label>Logradouro:</label>
-        <input type="text" name="logradouro" required>
+        <label for="cep">CEP:</label>
+        <input type="text" name="cep" id="cep" required><br>
 
-        <label>Número:</label>
-        <input type="text" name="numero" required>
+        <label for="logradouro">Logradouro:</label>
+        <input type="text" name="logradouro" id="logradouro" required><br>
 
-        <label>Complemento:</label>
-        <input type="text" name="complemento">
+        <label for="numero">Número:</label>
+        <input type="text" name="numero" id="numero" required><br>
 
-        <label>Bairro:</label>
-        <input type="text" name="bairro" required>
+        <label for="complemento">Complemento:</label>
+        <input type="text" name="complemento" id="complemento"><br>
 
-        <label>Estado:</label>
-        <input type="text" name="estado" required>
+        <label for="bairro">Bairro:</label>
+        <input type="text" name="bairro" id="bairro" required><br>
 
-        <label>Cidade:</label>
-        <input type="text" name="cidade" required>
+        <label for="estado">Estado:</label>
+        <input type="text" name="estado" id="estado" required><br>
+
+        <label for="cidade">Cidade:</label>
+        <input type="text" name="cidade" id="cidade" required><br>
 
         <button type="submit">Finalizar Cadastro</button>
     </form>
--- app/Views/cadastroPessoa.php
+++ app/Views/cadastroPessoa.php
@@ -1,22 +1,39 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Pessoa</title>
 </head>
+
 <body>
     <h1>Cadastro de Pessoa</h1>
     <form method="POST" action="index.php?action=cadastro">
+        <label for="nome">Nome:</label><br>
         <input type="text" name="nome" placeholder="Nome" required><br>
+
+        <label for="nome_social">Nome Social (opcional):</label> <br>
         <input type="text" name="nome_social" placeholder="Nome Social"><br>
+
+        <label for="cpf">CPF:</label> <br>
         <input type="text" name="cpf" placeholder="000000000-00" required><br>
+
+        <label for="nome_pai">Nome do Pai:</label> <br>
         <input type="text" name="nome_pai" placeholder="Nome do Pai"><br>
+
+        <label for="nome_mae">Nome da Mãe:</label> <br>
         <input type="text" name="nome_mae" placeholder="Nome da Mãe"><br>
-        <input type="text" name="telefone" placeholder="3199999-9999"><br>
+
+        <label for="telefone">Telefone:</label> <br>
+        <input type="tel" name="telefone" placeholder="3199999-9999"><br>
+
+        <label for="email">E-mail:</label> <br>
         <input type="email" name="email" placeholder="pessoa@gmail.com"><br>
 
         <button type="submit">Próximo</button>
     </form>
+
 </body>
+
 </html>
