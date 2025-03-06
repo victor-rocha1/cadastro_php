7f6d7c0f946a2f94afdbd633a689d9210ea78dc8
VICTOR ROCHA DOS REIS <victorrocha3202@gmail.com>
2025-03-06 11:23:53 -0300

    Refatorando o código

--- app/Views/cadastroEndereco.php
+++ app/Views/cadastroEndereco.php
@@ -13,25 +13,25 @@
 
         <input type="hidden" name="id_pessoa" value="<?php echo isset($_GET['id_pessoa']) ? $_GET['id_pessoa'] : ''; ?>">
 
-        <label for="cep">CEP:</label>
+        <label for="cep">CEP:</label><br>
         <input type="text" name="cep" id="cep" required><br>
 
-        <label for="logradouro">Logradouro:</label>
+        <label for="logradouro">Logradouro:</label><br>
         <input type="text" name="logradouro" id="logradouro" required><br>
 
-        <label for="numero">Número:</label>
+        <label for="numero">Número:</label><br>
         <input type="text" name="numero" id="numero" required><br>
 
-        <label for="complemento">Complemento:</label>
+        <label for="complemento">Complemento:</label><br>
         <input type="text" name="complemento" id="complemento"><br>
 
-        <label for="bairro">Bairro:</label>
+        <label for="bairro">Bairro:</label><br>
         <input type="text" name="bairro" id="bairro" required><br>
 
-        <label for="estado">Estado:</label>
+        <label for="estado">Estado:</label><br>
         <input type="text" name="estado" id="estado" required><br>
 
-        <label for="cidade">Cidade:</label>
+        <label for="cidade">Cidade:</label><br>
         <input type="text" name="cidade" id="cidade" required><br>
 
         <button type="submit">Finalizar Cadastro</button>
