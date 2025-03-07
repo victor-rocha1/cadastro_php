d396568a826d89436dc4e0f52261ae4c3443afae
VICTOR ROCHA DOS REIS <victorrocha3202@gmail.com>
2025-03-07 13:01:09 -0300

    modificando o css

--- app/Views/cadastroPessoa.php
+++ app/Views/cadastroPessoa.php
@@ -18,26 +18,26 @@
 <body>
     <h1>Cadastro de Pessoa</h1>
     <form method="POST" action="index.php?action=cadastro">
-        <label for="nome">Nome:</label><br>
+        <label for="nome">Nome:</label>
         <input type="text" name="nome" placeholder="Nome" required><br>
 
-        <label for="nome_social">Nome Social (opcional):</label> <br>
-        <input type="text" name="nome_social" placeholder="Nome Social (opcional)"><br>
+        <label for="nome_social">Nome Social (opcional):</label> 
+        <input type="text" name="nome_social" placeholder="Nome Social (opcional)">
 
-        <label for="cpf">CPF:</label> <br>
+        <label for="cpf">CPF:</label> 
         <input type="text" name="cpf" placeholder="000.000.000-00" required><br>
 
-        <label for="nome_pai">Nome do Pai:</label> <br>
-        <input type="text" name="nome_pai" placeholder="Nome do Pai (opcional)"><br>
+        <label for="nome_pai">Nome do Pai:</label> 
+        <input type="text" name="nome_pai" placeholder="Nome do Pai (opcional)">
 
-        <label for="nome_mae">Nome da Mãe:</label> <br>
-        <input type="text" name="nome_mae" placeholder="Nome da Mãe (opcional)"><br>
+        <label for="nome_mae">Nome da Mãe:</label> 
+        <input type="text" name="nome_mae" placeholder="Nome da Mãe (opcional)">
 
-        <label for="telefone">Telefone:</label> <br>
-        <input type="tel" name="telefone" placeholder="(31) 99999-9999"><br>
+        <label for="telefone">Telefone:</label> 
+        <input type="tel" name="telefone" placeholder="(31) 99999-9999">
 
-        <label for="email">E-mail:</label> <br>
-        <input type="email" name="email" placeholder="pessoa@gmail.com"><br>
+        <label for="email">E-mail:</label> 
+        <input type="email" name="email" placeholder="pessoa@gmail.com">
 
         <button type="submit">Próximo</button>
     </form>
